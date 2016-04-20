4769205df2c532d50965c4ce6391b1b53f29e15d
huhwhatyah <huhwhatyah@gmail.com>
2016-04-20 12:05:32 -0400

    Navigation Bar

--- web/static/js/react/pages/login-page.js
+++ web/static/js/react/pages/login-page.js
@@ -1,7 +1,15 @@
 var LoginPage = React.createClass({
   render() {
     return(
-      <div>Login placeholder</div>
+      <div>
+      Login placeholder
+      <ul>
+        <li><a href="#"><img src="/images/favicon.png" alt="RocketNanny" height="32"/></a></li>
+        <li><a href="#login">Login</a></li>
+        <li><a href="#signup">Signup</a></li>
+        <li><a><i className="fa fa-bars fa-2x" aria-hidden="true"></i></a></li>
+      </ul>
+      </div>
     );
   }
 });
--- web/static/js/react/pages/signup-page.js
+++ web/static/js/react/pages/signup-page.js
@@ -4,6 +4,12 @@ var SignupPage = React.createClass({
   render() {
     return(
       <div>
+      <ul>
+        <li><a href="#"><img src="/images/favicon.png" alt="RocketNanny" height="32"/></a></li>
+        <li><a href="#login">Login</a></li>
+        <li><a href="#signup">Signup</a></li>
+        <li><a><i className="fa fa-bars fa-2x" aria-hidden="true"></i></a></li>
+    </ul>
         Signup placeholder
         <SignupForm/>
       </div>
